6559928f3265973e851d032617d8598269ab1b57
GwakMirae <alfo9490@naver.com>
2021-05-23 02:33:16 +0900

    17.safeAreaView

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 
+// after `expo install react-native-safe-area-context`.
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default class App extends React.Component {
   render() {
     return (
+      <SafeAreaView style={styles.fullscreen}>
       <View style={styles.container}>
         <Image source={{
             uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
@@ -15,11 +18,15 @@ export default class App extends React.Component {
           }} />
         <StatusBar style="auto" />
       </View>
+    </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  fullscreen: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
