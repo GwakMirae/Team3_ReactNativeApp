68c40d90a47ec2d08c9c40d676c6bc118b067f89
Sinyoung3016 <tasha0415@naver.com>
2021-06-02 21:36:09 +0900

    Update WeatherDetailScreen : change View Text

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -38,28 +38,14 @@ export default class WeatherDetailScreen extends React.Component {
         route: {
           params: { city },
         },
-        navigation,
       } = this.props;
   
-      navigation.setOptions({ title: `Weather Information: ${city}` });
-
-    if (this.state.isLoading) {
       return (
         <View style={styles.container}>
-          <Text>데이터를 불러오는 중입니다.</Text>
+          <Text>{city}</Text>
         </View>
       )
     }
-
-    let celsius = this.state.main.temp - 273.15;
-
-    return (
-      <View style={styles.container}>
-        <Text>온도: {celsius.toFixed(1)}</Text>
-      </View>
-    );
-  }
-
 }
 
 const styles = StyleSheet.create({
